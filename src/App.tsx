0d8ba0de36b9993217361d026d5e6a68146c3119
Jean Grendel <jeanbgrendel@gmail.com>
2026-02-24 20:01:24 -0300

    Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Heading } from './components/Heading';
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App () {
   return (
   <>
-    <Heading />
+    <Heading>
+      Olá Mundo! 
+      <button>
+        <TimerIcon />
+        </button>
+      </Heading>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. 
       Deleniti, laborum tempora. Voluptatibus minus dolor in 
