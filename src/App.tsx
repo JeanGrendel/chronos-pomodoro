67019a37cf1070571283f7eb37911ef236800b65
JeanGrendel <jeanbgrendel@gmail.com>
2026-03-04 17:50:10 -0300

    Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-import { Heading } from './components/Heading';
 import { Container } from './components/Container';
+import { CountDown } from './components/CountDown';
+import DefaultInput from './components/DefaultInput';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 import './styles/global.css';
 import './styles/theme.css';
@@ -7,14 +10,36 @@ import './styles/theme.css';
 export function App() {
   return (
   <>
-    
-
     <Container>
-        <Heading>Logo</Heading>
+        <Logo />
     </Container>
 
     <Container>
-        <Heading>Menu</Heading>
+        <Menu />
+    </Container>
+
+    <Container>
+        <CountDown />
+    </Container>
+
+    <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput id='meuInput' type='text' labelText='Task' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
     </Container>
   </>
   );
---
+++ src/components/DefaultInput/index.tsx
@@ -0,0 +1,13 @@
+type DefaultInputProps = {
+  id: string;
+  labelText?: string;
+} & React.ComponentProps<'input'>;
+
+export default function DefaultInput({ id,type, labelText }: DefaultInputProps) {
+  return (
+    <>
+      {labelText ? <label htmlFor={id}>{labelText}</label> : ''}
+      <input id={id} type={type}></input> 
+    </>
+  );
+}
