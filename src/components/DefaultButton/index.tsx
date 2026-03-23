5d1f3df163a0b85e3f175819cc74d2d09247d78d
JeanGrendel <jeanbgrendel@gmail.com>
2026-03-23 17:32:28 -0300

    button

--- src/components/DefaultButton/index.tsx
+++ src/components/DefaultButton/index.tsx
@@ -2,12 +2,13 @@ import styles from './styles.module.css';
 
 type DefaultButtonProps = {
   icon: React.ReactNode;
+  color?: 'green' | 'red';
 } & React.ComponentProps<'button'>;
 
-export default function DefaultButton({ icon, ...props }: DefaultButtonProps) {
+export default function DefaultButton({ icon, color = 'green', ...props }: DefaultButtonProps) {
   return (
     <>
-      <button className={styles.button} {...props} >
+      <button className={`${styles.button} ${styles[color]}`} {...props} >
         {icon}
       </button> 
     </>
