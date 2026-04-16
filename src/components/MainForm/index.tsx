d8ae0a6735fe96b3a075bfea0135ffbb63cad465
JeanGrendel <jeanbgrendel@gmail.com>
2026-04-16 17:16:58 -0300

    Começando a lógica / Duration / Tipo com funções

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -4,9 +4,15 @@ import DefaultButton from "../DefaultButton";
 import DefaultInput from "../DefaultInput";
 import { useRef } from "react";
 import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleTipe } from "../../utils/getNextCycleTipe";
 
 export function MainForm() {
+  const { state, setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleTipe(nextCycle);
 
   function handleCreateNewTask(event: React.SubmitEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -26,9 +32,23 @@ export function MainForm() {
       startDate: Date.now(),
       completeDate: null,
       interruptDate: null,
-      duration: 1,
-      type: 'workTime',
+      duration: state.config[nextCycleType],
+      type: nextCycleType,
     };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining, // Conferir
+        formattedSecondsRemaining: '00:00', // Conferir
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
 
   }
 
