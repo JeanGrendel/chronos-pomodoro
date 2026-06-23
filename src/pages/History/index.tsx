9ed219ce5c7be2fbacf5d7f635250ae2d16210f0
JeanGrendel <jeanbgrendel@gmail.com>
2026-06-23 17:59:56 -0300

    histórico (Parcial)

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -5,8 +5,13 @@ import { Heading } from "../../components/Heading";
 import { MainTemplate } from "../../templates/MainTemplate";
 
 import styles from './styles.module.css';
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { formatDate } from "../../utils/formatDate";
+import { getTaskStatus } from "../../utils/getTaskStatus";
 
 export function History() {
+  const {state} = useTaskContext();
+
   return(
     <MainTemplate>
       <Container>
@@ -19,7 +24,33 @@ export function History() {
       </Container>
       
       <Container>
-        <div className="resposiveTable">dwlapflwp</div>
+        <div className={styles.responsiveTable}>
+          <table>
+            <thead>
+              <tr>
+                <th>Tarefa</th>
+                <th>Duração</th>
+                <th>Data</th>
+                <th>Status</th>
+                <th>Tipo</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {state.tasks.map(task => {
+                return (
+                  <tr key={task.id}>
+                    <td>{task.name}</td>
+                    <td> {task.duration}min</td>
+                    <td>{formatDate(task.startDate)}</td>
+                    <td>{getTaskStatus(task, state.activeTask)}</td>
+                    <td>{task.type}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
       </Container>
     </MainTemplate>
   );
