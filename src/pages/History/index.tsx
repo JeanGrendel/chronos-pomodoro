db08aad259dfaf7a28a2ba773b3b38bf65d9c14e
Jean Grendel <jeanbgrendel@gmail.com>
2026-07-06 21:09:44 -0300

    Final

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -53,6 +53,10 @@ export function History() {
     }
   }, []);
 
+  useEffect(() => {
+    document.title = 'Histórico - Chronos Pomodoro';
+  }, []);
+
   function handleSortTasks({ field }: Pick<SortTasksOptions, 'field'>) {
     const newDirection = sortTasksOptions.direction === 'desc' ? 'asc' : 'desc';
 
